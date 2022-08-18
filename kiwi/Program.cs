0ff67ea2c6db2a34708844f36524bc9f55c59821
Revel <GiAndF4TYYT@gmail.com>
2022-08-18 15:38:02 +0200

    hot fix

--- kiwi/Program.cs
+++ kiwi/Program.cs
@@ -7,7 +7,7 @@ class Program
         if (args.Length == 0)
         {
             Console.WriteLine("Invalid args:");
-            Console.WriteLine("Usage:\nkiwi [command] [args]");
+            Console.WriteLine("Usage > kiwi [command] [args]");
             Environment.Exit(1);
         }
 
